4def0936de7bb001b351aa82908d50911e2e5812
PaulZ <71932377+pawelZ2407@users.noreply.github.com>
2020-09-29 18:47:06 +0200

    Just to show that I did something

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+    [SerializeField] private GameObject[] meleeWeapon = new GameObject[3];
+    public GameObject[] MeleeWeapon
+    {
+        get { return meleeWeapon; }
+        set { meleeWeapon = value; }
+    }
+    [SerializeField] private GameObject weaponChanger;
+    private int choosedWeapon = 0;
+    private GameObject _currentWeapon;
+    public int ChoosedWeapon {
+        get { return choosedWeapon; }
+        set { choosedWeapon = value; }
+    }
+    private void Start() 
+    {
+        _currentWeapon = Instantiate(meleeWeapon[0]);
+    }
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            Destroy(_currentWeapon);
+            choosedWeapon = 0;
+            _currentWeapon = Instantiate(meleeWeapon[choosedWeapon]);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            Destroy(_currentWeapon);
+            choosedWeapon = 1;
+            _currentWeapon = Instantiate(meleeWeapon[choosedWeapon]);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            Destroy(_currentWeapon);
+            choosedWeapon = 2;
+            _currentWeapon = Instantiate(meleeWeapon[choosedWeapon]);
+        }
+        WeaponsChanger meleeWeapons = weaponChanger.GetComponent<WeaponsChanger>();
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(meleeWeapons.StartAttack(_currentWeapon));
+        }
+        else {
+            meleeWeapons.defaultPosition(_currentWeapon);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/WeaponsChanger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WeaponsChanger : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float attackSpeed = 0.2f;
+    [SerializeField] private float targetAngle = 100;
+    private bool inAttackMode = false;
+    private bool wasAttacked = false;
+    public void defaultPosition(GameObject currentWeapon) 
+        {
+        PlayerAttack playerAttack = player.GetComponent<PlayerAttack>();
+        currentWeapon.transform.position = player.transform.position;
+        currentWeapon.transform.parent = playerAttack.transform;
+        if (currentWeapon.CompareTag("Swipe")) 
+            {
+            Vector2 defaultPosLeft = new Vector2(-playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.x, playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.y);
+            Vector2 defaultPosRight = new Vector2(playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.x, playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.y);
+            Vector2 defaultPosUp = new Vector2(playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.x, playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.y);
+            Vector2 defaultPosDown = new Vector2(playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.x, playerAttack.MeleeWeapon[playerAttack.ChoosedWeapon].transform.localPosition.y);
+            if ( animator.GetBool("MovingRight") == true && inAttackMode == false)
+                {
+                currentWeapon.transform.localPosition = defaultPosRight;
+
+                Vector3 defaultRotation = new Vector3(0, 0, -30);
+                currentWeapon.transform.localEulerAngles = defaultRotation;
+            }
+            if ( animator.GetBool("MovingLeft") == true && inAttackMode == false) 
+                {
+                currentWeapon.transform.localPosition = defaultPosLeft;
+                Vector3 rotationLeft = new Vector3(0, 0, 30);
+                currentWeapon.transform.localEulerAngles = rotationLeft;
+            }
+        }   
+    }
+    public IEnumerator StartAttack(GameObject currentWeapon)
+        {
+        inAttackMode = true;
+        wasAttacked = true;
+        PlayerAttack playerAttack = player.GetComponent<PlayerAttack>();
+        currentWeapon.transform.parent = playerAttack.transform;
+        if (animator.GetBool("MovingRight") == true) 
+        {
+            currentWeapon.transform.localEulerAngles = new Vector3(0, 0, targetAngle);
+        }
+        else if(animator.GetBool("MovingLeft") == true) {
+            currentWeapon.transform.localEulerAngles = new Vector3(0, 0, -targetAngle);
+        }
+      /*  else if(animator.GetBool("MovingUp") == true) {
+            currentWeapon.transform.localEulerAngles = new Vector3(0, 0, -targetAngle);
+        }
+        else if(animator.GetBool("MovingDown") == true) {
+            currentWeapon.transform.localEulerAngles = new Vector3(0, 0, -targetAngle);
+        }*/
+        yield return new WaitForSeconds(attackSpeed / 2);
+        inAttackMode = false;
+        yield return new WaitForSeconds(attackSpeed);
+        wasAttacked = false;
+    }
+}
+
